48bcc3933a1eaefcce4239879b6f94f16ad13958
tianliqiang1 <tianliqiang1@rongcapital.cn>
2016-11-23 13:15:34 +0800

    edit my nodejs file.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 /**
  * Created by Administrator on 2016-11-23.
  */
+var http=require('http');
+
+http.createServer(function(req,res){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+}).listen(1337,'127.0.0.1');
